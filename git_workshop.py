479eaf5a3f8764b5410f733889cf56037ca8423b
sebastian <basti2013@outlook.com>
2023-11-02 19:37:59 -0700

    implement some functions

--- git_workshop.py
+++ git_workshop.py
@@ -2,21 +2,20 @@ import math
 
 # all functions should take in a numerical value and return a numerical value
 # rename your function
-def func1(num_a):
-    # placeholder return, replace with your own function
-    return -1
+def square(num_a):
+    return num_a * num_a
 
 def bananas(candy):
     # placeholder return, replace with your own function
     return candy * 2
 
 # both people should modify this function
-def fish_lover(num_a, num_b):
+def run(num_a, num_b):
     # do something with inputs and print something
-    print("hi")
+    print(f'i run {num_a} miles every {num_b} years')
 
 a = 1
 b = 1
-x = func1(a)
-y = bananas(b)
-fish_lover(x, y)
+x = square(a)
+y = func2(b)
+run(x, y)
